09418f81afe4e5f74c4dd6090675d3dee87473f7
Vishak11 <vishakvn007@gmail.com>
2024-01-19 18:58:54 +0530

    v3

--- codeEditor/src/components/Editor.jsx
+++ codeEditor/src/components/Editor.jsx
@@ -1,33 +1,36 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, } from "react";
 import Codemirror from "codemirror";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/dracula.css";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/addon/edit/closetag";
 import "codemirror/addon/edit/closebrackets";
-
 const Editor = () => {
-  const editorRef = useRef(null);
-  useEffect(() => {
-    async function init() {
-      console.log("init function is running");
-      editorRef.current = Codemirror.fromTextArea(
-        document.getElementById("realTimeEditor"),
-        {
+    const editorRef = useRef(null);
+  
+    useEffect(() => {
+      async function init() {
+        
+       editorRef.current = Codemirror.fromTextArea(document.getElementById("realTimeEditor"), {
           mode: { name: "javascript", json: true },
           theme: "dracula",
           autoClosetags: true,
           autoCloseBrackets: true,
           lineNumbers: true,
-        }
-      ).setSize("100%",756);
-      editorRef.current.on('change',(instance,changes)=>{
-        console.log(changes)
-      })
-    }
-    init();
-  }, []);
-  return <textarea  className="textEditor" id="realTimeEditor"></textarea>;
-};
+        })
+        editorRef.current.getWrapperElement().style.height = "756px";
 
-export default Editor;
+          editorRef.current.on('change',(instance,changes)=>{
+            console.log("changes",changes)
+          })
+        
+      }
+  
+      init();
+    }, []);
+  
+    return <textarea className="textEditor" id="realTimeEditor"></textarea>;
+  };
+  
+  export default Editor;
+  
